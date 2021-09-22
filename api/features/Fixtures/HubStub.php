bbf0e40bf52135df4a63eba49fe94312177fce17
Autsider <yorickvanklinken@gmail.com>
2021-09-22 18:44:18 +0200

    Added ci

---
+++ api/features/Fixtures/HubStub.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Fixtures;
+
+use Symfony\Component\Mercure\HubInterface;
+use Symfony\Component\Mercure\Jwt\StaticTokenProvider;
+use Symfony\Component\Mercure\Jwt\TokenFactoryInterface;
+use Symfony\Component\Mercure\Jwt\TokenProviderInterface;
+use Symfony\Component\Mercure\Update;
+
+final class HubStub implements HubInterface
+{
+    public function getUrl(): string
+    {
+        return 'url';
+    }
+
+    public function getPublicUrl(): string
+    {
+        return 'url';
+    }
+
+    public function getProvider(): TokenProviderInterface
+    {
+        return new StaticTokenProvider('token');
+    }
+
+    public function getFactory(): ?TokenFactoryInterface
+    {
+        return null;
+    }
+
+    public function publish(Update $update): string
+    {
+        return 'id';
+    }
+}
