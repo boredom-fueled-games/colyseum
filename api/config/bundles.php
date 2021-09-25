6b1f3978ecf64612c453785ce6183e8724d265b9
Autsider <yorickvanklinken@gmail.com>
2021-09-26 01:13:29 +0200

    Replacing theofidry/alice-data-fixtures with just basic nelmio/alice

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -17,5 +17,4 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
-    Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
